26db7b95aeb98111e856da1807ff08d56aead628
nanbuwks <nanbuwks+github@nanbu.com>
2023-12-19 01:22:48 +0900

    debug

--- githubaction/makegraph.R
+++ githubaction/makegraph.R
@@ -4,6 +4,6 @@ input_file <- args[1]
 output_file <- args[2]
 data <- read.csv(input_file)
 data <- dplyr::mutate(data, date = as.POSIXct(date,tz="Asia/Tokyo"))
-png(output_file, width = 480, height = 320)
+png(output_file, width = 300, height = 280)
 ggplot(aes(x = data[[3]], y = data[[4]]), data = data) + geom_bar(stat="identity")  + scale_y_continuous(expand = c(0, 0), limits = c(0, max(data[[4]])*1.1)) + xlab("")+ylab("μSv/h")
 dev.off() 
